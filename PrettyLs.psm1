a7fa34e1ff648b80aec4a5d67e4058d75a312814
Derek Zhang <derek.zhang0210@gmail.com>
2021-07-14 23:05:55 -0400

    Add icons for different file types

--- PrettyLs.psm1
+++ PrettyLs.psm1
@@ -4,10 +4,31 @@ function ls {
     foreach ($item in $directories){
         Write-Host $([char]0xe5ff) $item
     }
-    $files = Get-ChildItem -path . -file
+    $files = Get-ChildItem -path . -file -name
     foreach ($item in $files){
-        Write-Host $item
+        $split = $item.Split('.')
+        $ext = $split[$split.Count - 1]
+        if ($symbols.ContainsKey($ext)){
+            Write-Host $([char]$symbols[$ext]) $item
+        } else {
+            Write-Host $([char]0xf718) $item
+        }
     }
 }
+
+$symbols = @{
+    c = 0xe61e
+    cpp = 0xe61d
+    css = 0xe74a
+    java = 0xe738
+    js = 0xe74e
+    py = 0xe73c
+    psm1 = 0xe73c
+    rb = 0xe739
+    rs = 0xe7a8
+    scala = 0xe737
+    vim = 0xe61e
+}
+
 
 Export-ModuleMember -Function ls
